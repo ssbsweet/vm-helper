dc4ff968c136eb09095c5f6cc56885ef345257a2
ssbsweet <ssbsweet56@gmail.com>
2020-11-09 00:03:16 +0300

    ver.0.1.2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,26 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>HomePage</h1>
+            <v-flex xs12
+            v-for="(vm, i) in vms"
+            :key="i">
+                <v-banner single-line>
+                  <v-simple-table>
+                    <template v-slot:default>
+                      <tbody>
+                        <tr>
+                          <td>{{ vms.name }}</td>
+                          <td>{{ vms.task }}</td>
+                          <td>{{ vms.user }}</td>
+                          <td>{{ vms.date }}</td>
+                          <v-spacer></v-spacer>
+                          <v-btn>button1</v-btn>
+                          <v-btn>button2</v-btn>
+                        </tr>
+                      </tbody>
+                    </template>
+                  </v-simple-table>
+                </v-banner>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,9 +28,31 @@
 
 <script>
 export default {
-  computed: {
-    vms () {
-      return this.$store.getters.vms
+  data () {
+    return {
+      vms: [
+        {
+          id: '1',
+          name: 'vm1',
+          task: 'web-4550',
+          user: 'Артур',
+          date: '08.11.2020'
+        },
+        {
+          id: '2',
+          name: 'vm2',
+          task: 'apk-760',
+          user: 'Никита',
+          date: '08.11.2020'
+        },
+        {
+          id: '3',
+          name: 'vm3',
+          task: 'mob-721',
+          user: 'Мишаня',
+          date: '08.11.2020'
+        }
+      ]
     }
   }
 }
--- src/components/Vms/MyVMs.vue
+++ src/components/Vms/MyVMs.vue
@@ -1,8 +1,26 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>MyVMs</h1>
+            <v-flex xs12
+            v-for="(vm, i) in vms"
+            :key="i">
+                <v-banner single-line>
+                  <v-simple-table>
+                    <template v-slot:default>
+                      <tbody>
+                        <tr>
+                          <td>{{ vm.name }}</td>
+                          <td>{{ vm.task }}</td>
+                          <td>{{ vm.user }}</td>
+                          <td>{{ vm.date }}</td>
+                            <v-spacer></v-spacer>
+                            <v-btn dark color="light-blue darken-4">b1</v-btn>
+                            <v-btn dark color="light-blue darken-4">b2</v-btn>
+                        </tr>
+                      </tbody>
+                    </template>
+                  </v-simple-table>
+                </v-banner>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,9 +28,31 @@
 
 <script>
 export default {
-  computed: {
-    vms () {
-      return this.$store.getters.vms
+  data () {
+    return {
+      vms: [
+        {
+          id: '1',
+          name: 'vm1',
+          task: 'web-4550',
+          user: 'Артур',
+          date: '08.11.2020'
+        },
+        {
+          id: '2',
+          name: 'vm2',
+          task: 'apk-760',
+          user: 'Никита',
+          date: '08.11.2020'
+        },
+        {
+          id: '3',
+          name: 'vm3',
+          task: 'mob-721',
+          user: 'Мишаня',
+          date: '08.11.2020'
+        }
+      ]
     }
   }
 }
