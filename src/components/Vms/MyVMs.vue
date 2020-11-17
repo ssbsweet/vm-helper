6c5b3b73b118a46981efd7cfb06b70dc427405c6
Mikhail Bazylevich <ssbsweet56@gmail.com>
2020-11-17 16:22:00 +0300

    111
    21

--- src/components/Vms/MyVMs.vue
+++ src/components/Vms/MyVMs.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-container>
       <v-layout row>
           <v-flex xs12
@@ -25,33 +24,6 @@
           </v-flex>
       </v-layout>
   </v-container>
-=======
-    <v-container>
-        <v-layout row>
-            <v-flex xs12
-            v-for="(vm, i) in vms"
-            :key="i">
-                <v-banner single-line>
-                  <v-simple-table>
-                    <template v-slot:default>
-                      <tbody>
-                        <tr>
-                          <td>{{ vm.name }}</td>
-                          <td>{{ vm.task }}</td>
-                          <td>{{ vm.owner }}</td>
-                          <td>{{ vm.date }}</td>
-                            <v-spacer></v-spacer>
-                            <edit-vm :vm="vm"></edit-vm>
-                            <v-btn dark color="light-blue darken-4">b2</v-btn>
-                        </tr>
-                      </tbody>
-                    </template>
-                  </v-simple-table>
-                </v-banner>
-            </v-flex>
-        </v-layout>
-    </v-container>
->>>>>>> parent of 5de59fe... upd 0.4.5
 </template>
 
 <script>
@@ -66,7 +38,6 @@ export default {
   },
   components: {
     EditVm
-<<<<<<< HEAD
   },
   methods: {
     onDelete () {
@@ -76,8 +47,6 @@ export default {
     isOwner () {
       return this.product.ownerId === this.$store.getters.user.id
     }
-=======
->>>>>>> parent of 5de59fe... upd 0.4.5
   }
 }
 </script>
