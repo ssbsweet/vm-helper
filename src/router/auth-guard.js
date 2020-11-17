ab8c6ab80827eedc6c1e7ba4a045655b1a0e08a7
ssbsweet <ssbsweet56@gmail.com>
2020-11-17 21:47:41 +0300

    Update auth-guard.js

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,9 +1,21 @@
 import store from '../store/index'
 
+// export default function (to, from, next) {
+//   if (store.getters.user) {
+//     next()
+//   } else {
+//     next('/?loginError=true')
+//   }
+// }
+
 export default function (to, from, next) {
-  if (store.getters.user) {
-    next()
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.user) {
+      next()
+    } else {
+      next('/?loginError=true')
+    }
   } else {
-    next('/?loginError=true')
+    next()
   }
 }
